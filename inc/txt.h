299447f483221d8b079fea482bf3782bd86b9bbc
John Pritchard <logicalexistentialism@gmail.com>
2024-02-28 19:18:50 -0500

    review

---
+++ inc/txt.h
@@ -0,0 +1,15 @@
+/*
+ * syntelos common text operators
+ * Copyright (C) 2024, John Pritchard, Syntelos
+ */
+#ifndef _txt_h
+#define _txt_h
+
+#include "txt_cc.h"
+#include "txt_file.h"
+#include "txt_path.h"
+#include "txt_span_group.h"
+#include "txt_string.h"
+#include "txt_symbol.h"
+#include "txt_text.h"
+#endif
